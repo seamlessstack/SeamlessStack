cd7598cf2bfd7abef86c0dce087e0165e4b8821e
Anand Bhat <anand.bhat@interactivity.co.in>
2013-07-17 23:35:47 +0530

    Defined job and transport structures. Transport structures will be moved to new file once there is more clarity.

--- common/include/sstack_jobs.h
+++ common/include/sstack_jobs.h
@@ -22,6 +22,9 @@
 
 #define SFSD_MAGIC 0x11101974
 #include <stdint.h>
+#define MAX_SFSD_CLIENTS 8
+#define IPV4_ADDR_MAX 16
+#define IPV6_ADDR_MAX 40
 
 typedef uint64_t sfsd_client_handle_t;
 typedef uint8_t sfsd_payload_t;
@@ -31,13 +34,65 @@ typedef enum {
 	SFSD_NFS		= 2,
 } sfsd_job_type_t;
 
+typedef enum {
+	JOB_STARTED		= 1,
+	JOB_COMPLETE	= 2,
+	JOB_ABORTED		= 3,
+	JOB_FAILED		= 4,
+} sfsd_job_status_t;
+
 
 typedef struct job {
-	int	version;
-	sfsd_client_handle_t client_handle;	// Client identifier
+	int version; 
 	sfsd_job_type_t job_type;
-	int	payload_len;
-	sfsd_payload_t	payload;
-} job_t;
+	int num_clients; // Number of valid clients
+	sfsd_t sfsd_t[MAX_SFSD_CLIENTS];
+	sfsd_job_status_t job_status[MAX_SFSD_CLIENTS]; // Status of each client
+	int payload_len;
+	sfsd_payload_t	payload[0];
+};
+
+typedef enum {
+	TCPIP		= 1,
+	IB			= 2,
+	UDP			= 3,
+	UNIX		= 4,
+} sfsd_transport_type_t;
+
+typedef struct sfsd_transport {
+	sfsd_transport_type_t  transport_type;
+	union {
+		struct tcp {
+			int ipv4;		// 1 for ipv4 and 0 for ipv6
+			union {
+				char ipv4_addr[IPV4_ADDR_MAX];
+				char ipv6_addr[IPV6_ADDR_MAX];
+			};
+			int port;
+		};
+	};
+} sfsd_transport_t;
+
+/*
+ *  init() is supposed to establish a connection and retrun client handle
+ *  Client handle is socket fd i case of TCPIP
+ */
+typedef struct sfsd_transport_ops {
+	int (*init) (sfsd_transport_t *);
+	int (*tx) (sfsd_client_handle_t , int , char *); 
+	int (*rx) (sfsd_cleint_handle_t , int, char *);
+} sfsd_transport_ops_t;
+
+typedef struct sfsd {
+	sfsd_transport_t sfsd_transport;
+	sfsd_transport_ops_t sfsd_transport_ops;
+	sfsd_client_handle_t sfsd_handle;
+} sfsd_t;
+
+
+
+// Different types of payloads to be defined here
+// TBD
+
 
 #endif // __SSTACK_JOBS_H_
