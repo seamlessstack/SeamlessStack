ecad4a0731efdaf54bb51186edb58d8984ea1492
Anand Bhat <anand.bhat@interactivity.co.in>
2013-07-20 13:54:07 +0530

    Fixed set_bit warning. Added two functions in sstack_helper.h

---
+++ lib/idp/include/idp.h
@@ -0,0 +1,31 @@
+/*************************************************************************
+ * 
+ * SEAMLESSSTACK CONFIDENTIAL
+ * __________________________
+ * 
+ *  [2012] - [2013]  SeamlessStack Inc
+ *  All Rights Reserved.
+ * 
+ * NOTICE:  All information contained herein is, and remains
+ * the property of SeamlessStack Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to SeamlessStack Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from SeamlessStack Incorporated.
+ */
+
+
+#ifndef __IDP_H_
+#define __IDP_H_
+
+static inline int
+get_path_name(char *path_name, char *dir_name, char *d_name)
+{
+
+	return 0;
+}
+
+#endif // __IDP_H_
