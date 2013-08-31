d2205d8474ea2df79837ec1b04eacfd52e6c903f
Anand Bhat <anand.bhat@interactivity.co.in>
2013-08-31 23:57:49 +0530

    Implemented one command in serdes (serial- deserial ) library. Lot more to implement. Removed protobuf *.[ch] files and modified Makefile to compile them. There are issues in jobs.proto stat message structure. I had to rename st_atime, st_mtime and st_ctime to st_atime1, st_mtime1 and st_ctime1 resply. Not sure why this is happening. There is no way to include standard header file in Google protobug impl.
    Fixed Makefile to compile whole thing (except sfs :-))

--- Makefile
+++ Makefile
@@ -27,17 +27,29 @@ COMMONDIR = common
 CACHINGDIR = lib/caching
 COMPRESSION_PLUGIN = lib/storage/plugins/compression
 OSS_INSTALL_DIR = $(PWD)/oss_install
+PROTOBUF_DIR = $(PWD)/protobuf-template/proto
+SERDES_DIR = $(PWD)/lib/serdes
 MKDIR = mkdir
+PROTOC = protoc-c
+LN = ln -s
 
 all:
 	$(MKDIR) -p $(OSS_INSTALL_DIR)
 	$(MAKE) -C $(OSS)
+	# Following dirty hack is because --proto_path does not work !!
+	$(LN) $(PROTOBUF_DIR)/jobs.proto jobs.proto
+	$(PROTOC) --c_out=$(PROTOBUF_DIR) jobs.proto
+	$(RM) -f jobs.proto
+	$(LN) $(PROTOBUF_DIR)/cli.proto cli.proto
+	$(PROTOC) --c_out=$(PROTOBUF_DIR) cli.proto
+	$(RM) -f cli.proto
 	$(MAKE) -C $(MONGODIR)
 	$(MAKE) -C $(MONGODRV)
 	$(MAKE) -C $(POLICYDIR)
 	$(MAKE) -C $(CLIDIR)
 	$(MAKE) -C $(CACHINGDIR)
 	$(MAKE) -C $(COMPRESSION_PLUGIN)
+	$(MAKE) -C $(SERDES_DIR)
 	$(MAKE) -C $(COMMONDIR)
 	$(MAKE) -C $(SFSDDIR)
 	$(MAKE) -C $(SFSDIR)
@@ -50,7 +62,10 @@ clean:
 	$(MAKE) -C $(CLIDIR) clean
 	$(MAKE) -C $(CACHINGDIR) clean
 	$(MAKE) -C $(COMPRESSION_PLUGIN) clean
+	$(MAKE) -C $(SERDES_DIR) clean
 	$(MAKE) -C $(SFSDDIR) clean
 	$(MAKE) -C $(SFSDIR) clean
 	$(MAKE) -C $(COMMONDIR) clean
 	$(RM) -rf $(OSS_INSTALL_DIR)
+	$(RM) -f $(PROTOBUF_DIR)/jobs.pb-c.h  $(PROTOBUF_DIR)/jobs.pb-c.c
+	$(RM) -f $(PROTOBUF_DIR)/cli.pb-c.h  $(PROTOBUF_DIR)/cli.pb-c.c
---
+++ lib/serdes/include/sstack_serdes.h
@@ -0,0 +1,45 @@
+/*************************************************************************
+ * 
+ * SEAMLESSSTACK CONFIDENTIAL
+ * __________________________
+ * 
+ *  [2012] - [2013]  SeamlessStack Inc
+ *  All Rights Reserved.
+ * 
+ * NOTICE:  All information contained herein is, and remains
+ * the property of SeamlessStack Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to SeamlessStack Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from SeamlessStack Incorporated.
+ */
+
+#ifndef __SSTACK_SERDES_H_
+#define __SSTACK_SERDES_H_
+
+#include <stdint.h>
+#include <sstack_jobs.h>
+#include <sstack_transport.h>
+
+/*
+ * sstack_send_payload - Function that serializes payload and sends
+ *		it across to the reciver
+ */
+
+extern int sstack_send_payload(sstack_client_handle_t handle,
+				sstack_payload_t *payload, sstack_transport_t *transport,
+				log_ctx_t *ctx);
+
+/*
+ * sstack_recv_payload - Function that receives a payload from transport
+ * 		and deserializes it before handing it over to the receiver
+ */
+
+extern sstack_payload_t * sstack_recv_payload(sstack_client_handle_t handle,
+				sstack_transport_t *transport, log_ctx_t *ctx);
+
+#endif // __SSTACK_SERDES_H_	
+
