ac60f946509b67bbbe5ec88b30d208dd0f5bd435
Anand Bhat <anand.bhat@interactivity.co.in>
2013-10-16 00:53:21 +0530

    Modified test pgm

--- lib/caching/test/test.c
+++ lib/caching/test/test.c
@@ -34,7 +34,7 @@ main(void)
 	char *ptr = NULL;
 	int len = -1;
 
-	memc = sstack_cache_init(config, 1);
+	memc = sstack_cache_init(config, 1, NULL);
 	if (NULL == memc) {
 		fprintf(stderr, "%s: Failed to get memcached handle\n", __FUNCTION__);
 		return -1;
@@ -42,14 +42,14 @@ main(void)
 	fprintf(stdout, "%s: Connection succeeded \n", __FUNCTION__);
 	// Store an object
 	fprintf(stdout, "%s: Storing %s\n", __FUNCTION__, value);
-	ret = sstack_cache_store(memc, key, value, strlen(value)+1);	
+	ret = sstack_cache_store(memc, key, value, strlen(value)+1, NULL);	
 	if (ret != 0) {
 		fprintf(stderr, "%s: Failed to store the object into memcached."
 			" Return value = %d\n", __FUNCTION__, ret);
 		return -1;
 	}
 	// Read back the value
-	ptr = sstack_cache_read_one(memc, key, strlen(key), (size_t *)&len);
+	ptr = sstack_cache_read_one(memc, key, strlen(key), (size_t *)&len, NULL);
 	if (NULL == ptr) {
 		fprintf(stderr, "%s: Failed to read back the value stored. \n",
 			__FUNCTION__);
@@ -60,14 +60,14 @@ main(void)
 
 	// Replace the value stored for the key
 	ret = sstack_cache_replace(memc, key, repvalue, (size_t )strlen(repvalue)+1,
-		 (time_t)0, (uint32_t)0);
+		 (time_t)0, (uint32_t)0, NULL);
 	if (ret != 0) {
 		fprintf(stderr, "%s: Failed to replace the value stored. \n",
 			__FUNCTION__);
 		return -1;
 	}
 	// Re-read and display
-	ptr = sstack_cache_read_one(memc, key, strlen(key), (size_t *) &len);
+	ptr = sstack_cache_read_one(memc, key, strlen(key), (size_t *) &len, NULL);
 	if (NULL == ptr) {
 		fprintf(stderr, "%s: Failed to read back the value stored. \n",
 			__FUNCTION__);
@@ -76,7 +76,7 @@ main(void)
 	fprintf(stderr, "%s: Value length = %d value = %s\n",
 		__FUNCTION__, len, ptr);
 	
-	sstack_cache_destroy(memc);
+	sstack_cache_destroy(memc, NULL);
 
 	return 0;
 }
