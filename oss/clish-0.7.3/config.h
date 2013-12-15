938c58632455909183606e997ea60fd1f1645ea5
Shubhro Sinha <shubhro.sinha@interactivity.co.in>
2013-12-15 13:31:37 +0530

    Framework for cli using clish

---
+++ oss/clish-0.7.3/config.h
@@ -0,0 +1,92 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <bfd.h> header file. */
+/* #undef HAVE_BFD_H */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Have Binary File Decription library */
+/* #undef HAVE_LIBBFD */
+
+/* Have native langauage support library */
+/* #undef HAVE_LIBINTL */
+
+/* Have POSIX threads library */
+#define HAVE_LIBPTHREAD 
+
+/* Have POSIX real time library */
+#define HAVE_LIBRT 
+
+/* Have TCL library */
+/* #undef HAVE_LIBTCL */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <pthread.h> header file. */
+#define HAVE_PTHREAD_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <tcl.h> header file. */
+/* #undef HAVE_TCL_H */
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "clish"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "gmckerrell at users dot sourceforge dot net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "clish"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "clish 0.7.3"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "clish"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.7.3"
+
+/* The size of `int', as computed by sizeof. */
+#define SIZEOF_INT 4
+
+/* The size of `long', as computed by sizeof. */
+#define SIZEOF_LONG 8
+
+/* The size of `size_t', as computed by sizeof. */
+#define SIZEOF_SIZE_T 8
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.7.3"
