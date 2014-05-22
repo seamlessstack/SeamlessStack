94ac3abd9bafd59b756342069559c45ac1dc17e2
Shubhro Sinha <shubhro.sinha@interactivity.co.in>
2014-05-22 22:00:19 +0530

    more dependencies

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -2,4 +2,4 @@
 # Installs the packages necessary to build sstack on Ubuntu
 sudo apt-get install g++ libevent-dev protobuf-c-compiler libprotobuf-c0-dev \
 liblzma-dev mongodb-dev libjudy-dev zlib1g-dev yasm libssl-dev libfuse-dev \
-libattr1-dev libgflags-dev libzmq3 libzmq333-dev
+libattr1-dev libgflags-dev libzmq3 libzmq3-dev libtool autoconf automake
